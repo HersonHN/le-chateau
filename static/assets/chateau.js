ab427e5cd39fb19322bfa1621df563a452ba466b
Herson Salinas <hersonhn@gmail.com>
2019-12-14 00:07:34 -0600

    test button

--- static/assets/chateau.js
+++ static/assets/chateau.js
@@ -2,6 +2,29 @@
 
 const socket = io();
 
-socket.on('msg', (message) => {
+window.onload = init;
+
+
+function init() {
+    bindSocket();
+    bindDOM();
+}
+
+function bindSocket() {
+    socket.on('msg', (msg) => logMessage(msg));
+}
+
+function bindDOM() {
+    const button = document.querySelector('.say-hello');
+    button.addEventListener('click', emitMessage);
+}
+
+function logMessage(message) {
     console.log('message:', message);
-});
+}
+
+function emitMessage() {
+    console.log('saying hi...');
+    socket.emit('msg', 'hello world');
+}
+
