5ea480d147c55e883c17611fdd7fe2ab17b85155
Herson Salinas <hersonhn@gmail.com>
2019-12-15 01:46:19 -0600

    homepage

---
+++ static/assets/sections/index-page.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// <section class="message-input">
+//     <input class="content" type="text" maxlength="40" />
+//     <button class="send">Go</button>
+// </section>
+
+window.onload = init;
+
+function init() {
+    const $button = document.querySelector('.message-input .send');
+    const $input = document.querySelector('.message-input .content');
+
+    $button.addEventListener('click', () => {
+        visitChatroom($input.value);
+    });
+
+    $input.addEventListener('keyup', (e) => {
+        if (e.keyCode === 13) {
+            visitChatroom($input.value);
+        }
+    });
+}
+
+function visitChatroom(room) {
+    room = room.replace(/\s/g, '-').toLowerCase();
+    console.log('room', room);
+    window.location.pathname = `/room/${room}`;
+}
