75e2929b0b0ef440a56e29ff9742a5aeacc7c7f3
Herson Salinas <hersonhn@gmail.com>
2019-12-14 23:35:34 -0600

    compile the message in the backend

--- controllers/socket-contoller.js
+++ controllers/socket-contoller.js
@@ -1,13 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+const nunjucks = require('nunjucks');
+
+const file = path.join(__dirname, '../templates/partials/message.html');
+const templateTXT = fs.readFileSync(file, { encoding: 'utf-8' });
+var template = nunjucks.compile(templateTXT);
 
 module.exports = function SocketController(socket) {
 
     socket.on('join', function(room) {
         socket.join(room);
 
-        socket.on('message', (message) => {
-            console.log(message)
-            socket.to(room).emit('message', message);
+        socket.on('message', function(message) {
+
+            message.timestamp = timestamp();
+            let id = socket.id;
+            let messageHTML = template.render({ message });
+            
+            socket.to(room).emit('message', messageHTML);
+            socket.to(id).emit('message', messageHTML);
+            // io.to().emit('hey', 'I just met you');
         });
     });
 
+}
+
+
+function timestamp() {
+    return +(new Date());
 }
--- static/assets/chateau.js
+++ static/assets/chateau.js
@@ -2,6 +2,7 @@
 
 const socket = io();
 
+window.username = 'herson'; // testing
 window.onload = init;
 
 
@@ -18,17 +19,33 @@ function bindSocket() {
 }
 
 function bindDOM() {
-    const button = document.querySelector('.say-hello');
-    button.addEventListener('click', emitMessage);
+    const $button = document.querySelector('.message-input .send');
+    const $input = document.querySelector('.message-input .content');
+
+    $button.addEventListener('click', emitMessage);
+    $input.addEventListener('keyup', (e) => {
+        if (e.keyCode === 13) {
+            emitMessage();
+        }
+    });
 }
 
 function logMessage(message) {
     console.log('message:', message);
+    let $messages = document.querySelector('.messages');
+    let div = document.createElement('div');
+    div.innerHTML = message;
+    $messages.append(div);
 }
 
 function emitMessage() {
-    console.log('saying hi...');
-    socket.emit('message', { content: 'hello world', room: getRoomName() });
+    const $input = document.querySelector('.message-input .content');
+    const message = $input.value.trim();
+    const author = window.username;
+    
+    if (!message) return;
+    $input.value = '';
+    socket.emit('message', { message, author });
 }
 
 function getRoomName() {
