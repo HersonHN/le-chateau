dd62a8777634cdd982308868d0162ccb472981bb
Herson Salinas <hersonhn@gmail.com>
2019-12-14 23:46:25 -0600

    send message to self

--- controllers/socket-contoller.js
+++ controllers/socket-contoller.js
@@ -6,20 +6,24 @@ const file = path.join(__dirname, '../templates/partials/message.html');
 const templateTXT = fs.readFileSync(file, { encoding: 'utf-8' });
 var template = nunjucks.compile(templateTXT);
 
+
 module.exports = function SocketController(socket) {
 
     socket.on('join', function(room) {
         socket.join(room);
 
-        socket.on('message', function(message) {
+        socket.on('message', function (message) {
 
             message.timestamp = timestamp();
             let id = socket.id;
             let messageHTML = template.render({ message });
             
+            // send the message to all other members of the room
             socket.to(room).emit('message', messageHTML);
-            socket.to(id).emit('message', messageHTML);
-            // io.to().emit('hey', 'I just met you');
+
+            // send the message to the socket itseft
+            this.emit('message', messageHTML);
+            
         });
     });
 
